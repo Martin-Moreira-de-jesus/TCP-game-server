e7e0cd6f47b952285ceccd50eb09bde69662a44b
adrien.raimbault <adrien.raimbault@etu.u-montpellier.fr>
2023-02-08 16:38:18 +0100

    Fix(tcpClient) : read from server now

--- client/clientTCP.go
+++ client/clientTCP.go
@@ -9,43 +9,40 @@ import (
 var wg sync.WaitGroup
 
 func main() {
-	for {
-		message := "Halo"
-		servAddr := "localhost:3000"
-		tcpAddr, err := net.ResolveTCPAddr("tcp", servAddr)
-		if err != nil {
-			println("ResolveTCPAddr failed:", err.Error())
-			os.Exit(1)
-		}
 
-		conn, err := net.DialTCP("tcp", nil, tcpAddr)
-		if err != nil {
-			println("Dial failed:", err.Error())
-			os.Exit(1)
-		}
-
-		println("write to server = ", message)
-		wg.Add(1)
-		go tcpRead(conn, err)
-		wg.Add(1)
-		go tcpWrite(conn, err, message)
-
-		wg.Wait()
+	servAddr := "localhost:3000"
+	tcpAddr, err := net.ResolveTCPAddr("tcp", servAddr)
+	if err != nil {
+		println("ResolveTCPAddr failed:", err.Error())
+		os.Exit(1)
 	}
+
+	conn, err := net.DialTCP("tcp", nil, tcpAddr)
+	if err != nil {
+		println("Dial failed:", err.Error())
+		os.Exit(1)
+	}
+
+	wg.Add(1)
+	go tcpRead(conn, err)
+	//go tcpWrite(conn, err, message)
+	wg.Wait()
 }
 
 func tcpRead(conn *net.TCPConn, err error) {
 	defer wg.Done()
-	received := make([]byte, 1024)
-	_, err = conn.Read(received)
-	if err != nil {
-		println("Read from server failed:", err.Error())
-		os.Exit(1)
+	for {
+		received := make([]byte, 1024)
+		_, err = conn.Read(received)
+		if err != nil {
+			println("Read from server failed:", err.Error())
+			os.Exit(1)
+		}
+		println(received)
 	}
 }
 
 func tcpWrite(conn *net.TCPConn, err error, message string) {
-	defer wg.Done()
 	_, err = conn.Write([]byte(message))
 	if err != nil {
 		println("Write to server failed:", err.Error())
