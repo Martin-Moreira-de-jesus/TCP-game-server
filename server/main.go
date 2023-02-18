daf1472a29eb717e9a6c9a6e06d6302d0e0ca930
adrien.raimbault <adrien.raimbault@etu.u-montpellier.fr>
2023-02-18 20:01:30 +0100

    Fix (server) : quick fixes to move characters

--- server/main.go
+++ server/main.go
@@ -1,26 +1,26 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
 	"log"
-    "math/rand"
-    "net"
+	"math/rand"
+	"net"
 	"strconv"
-    "strings"
-    "sync"
+	"strings"
+	"sync"
 	"time"
 )
 
 type PlayerState struct {
-    mu   sync.Mutex
-    up   bool
-    down bool
+	mu   sync.Mutex
+	up   bool
+	down bool
 }
 
 var cn = make(chan *Game)
 
 func handleConnection(conn net.Conn) {
-    defer conn.Close()
+	defer conn.Close()
 	if QuickWrite(conn, "Joining lobby...") != nil {
 		return
 	}
@@ -44,9 +44,9 @@ func handleConnection(conn net.Conn) {
 		time.Sleep(1 * time.Second)
 	}
 
-    fmt.Println("Attempting to start game...")
+	fmt.Println("Attempting to start game...")
 
-    game.val.LaunchGameLoopIfNotRunning(cn)
+	game.val.LaunchGameLoopIfNotRunning(cn)
 
 	if QuickWrite(conn, "Game starting !") != nil {
 		return
@@ -54,16 +54,16 @@ func handleConnection(conn net.Conn) {
 
 	State.mu.Lock()
 
-    var otherPlayer *Node[Player]
+	var otherPlayer *Node[Player]
 	for e := game.val.players.First(); e != nil; e = e.Next() {
-        if e.val.uuid != youPlayer.val.uuid {
-            otherPlayer = e
-        }
-    }
+		if e.val.uuid != youPlayer.val.uuid {
+			otherPlayer = e
+		}
+	}
 
 	State.mu.Unlock()
 
-    go handleUserInput(conn, youPlayer)
+	go handleUserInput(conn, youPlayer)
 
 	for {
 		State.mu.Lock()
@@ -73,9 +73,9 @@ func handleConnection(conn net.Conn) {
 				"you=%s,other=%s,pipeX=%s,obstacleY1=%s,obstacleY2=%s",
 				strconv.Itoa(youPlayer.val.posY),
 				strconv.Itoa(otherPlayer.val.posY),
-                strconv.Itoa(game.val.obstacleX),
-                strconv.Itoa(game.val.obstacleY1),
-                strconv.Itoa(game.val.obstacleY2),
+				strconv.Itoa(game.val.obstacleX),
+				strconv.Itoa(game.val.obstacleY1),
+				strconv.Itoa(game.val.obstacleY2),
 			),
 		) != nil {
 			return
@@ -86,70 +86,76 @@ func handleConnection(conn net.Conn) {
 }
 
 func handleUserInput(conn net.Conn, player *Node[Player]) {
-    defer conn.Close()
-    for {
-        var message string
-        if QuickRead(conn, &message) == nil {
-            return
-        }
+	defer conn.Close()
+	for {
+		var message string
+		if QuickRead(conn, &message) != nil {
+			println("null")
+			return
+		}
 
-        State.mu.Lock()
-        var values = strings.Split(message, ",")
-        for _, e := range values {
-            var data = strings.Split(e, "=")
-            if data[0] == "up" {
-                player.val.up, _ = strconv.ParseBool(data[1])
-            } else if data[1] == "down" {
-                player.val.down, _ = strconv.ParseBool(data[1])
-            }
-        }
-        State.mu.Unlock()
-    }
+		State.mu.Lock()
+		var values = strings.Split(message, ",")
+		println(values[0], "ok")
+		for _, e := range values {
+			var data = strings.Split(e, "=")
+			if data[0] == "up" {
+				player.val.up,_ = strconv.ParseBool(data[1])
+				println(player.val.up)
+				player.val.posY += 10 // Adding values directly here
+			} else if data[0] == "down" {
+				player.val.down, _ = strconv.ParseBool(data[1])
+				player.val.posY -= 10 // Adding values directly here
+			}
+		}
+		State.mu.Unlock()
+	}
 }
 
 func handleGameloop() {
-    var game = <- cn
-    fmt.Println("Starting game loop !")
-    for {
-        State.mu.Lock()
+	var game = <-cn
+	fmt.Println("Starting game loop !")
+	for {
+		State.mu.Lock()
 
-        // edit game
-        game.obstacleX -= 10
+		// edit game
+		game.obstacleX -= 10
 
-        if game.obstacleX <= 0 {
-            game.obstacleX = 1000
-            game.obstacleY1 = rand.Intn(800)
-            game.obstacleY2 = game.obstacleY1 + 100
-        }
-
-        // edit players
-        for player := game.players.First(); player != nil; player = player.Next() {
-            if player.val.up {
-                player.val.posY += 10
-            } else if player.val.down {
-                player.val.posY += 10
-            }
-        }
-
-        State.mu.Unlock()
-        time.Sleep(50 * time.Millisecond)
-    }
+		if game.obstacleX <= 0 {
+			game.obstacleX = 1000
+			game.obstacleY1 = rand.Intn(800)
+			game.obstacleY2 = game.obstacleY1 + 100
+		}
+/*
+		// edit players
+		for player := game.players.First(); player != nil; player = player.Next() {
+			println("test ", player.val.up)
+			if player.val.up == "true" {
+				player.val.posY += 10
+			} else if player.val.down {
+				player.val.posY += 10
+			}
+		}
+*/
+		State.mu.Unlock()
+		time.Sleep(50 * time.Millisecond)
+	}
 }
 
 func main() {
-    if true {
-        ln, err := net.Listen("tcp", ":8080")
-        if err != nil {
-            log.Fatal(err)
-        }
-        go handleGameloop()
-        for {
-            conn, err := ln.Accept()
-            if err != nil {
-                // handle error
-            }
+	if true {
+		ln, err := net.Listen("tcp", ":8080")
+		if err != nil {
+			log.Fatal(err)
+		}
+		go handleGameloop()
+		for {
+			conn, err := ln.Accept()
+			if err != nil {
+				// handle error
+			}
 
-            go handleConnection(conn)
-        }
-    }
+			go handleConnection(conn)
+		}
+	}
 }
