59bec03574cd200b9f2253a5319aa91b18643167
adrien.raimbault <adrien.raimbault@etu.u-montpellier.fr>
2023-02-25 12:44:16 +0100

    Fix (clientTCP) : enable multiplayer and  Read/Write operations to server

--- client/clientTCP.go
+++ client/clientTCP.go
@@ -6,44 +6,68 @@ import (
 	"sync"
 )
 
+type SafeCounter struct {
+	mu          sync.Mutex
+	instruction []string
+}
+
+type ClientInfos struct {
+	conn     *net.TCPConn
+	servAddr string
+}
+
+var State = ClientInfos{
+	conn: &net.TCPConn{},
+}
+
 var wg sync.WaitGroup
 
-func main() {
+func (ci ClientInfos) Client() {
 
-	servAddr := "localhost:3000"
-	tcpAddr, err := net.ResolveTCPAddr("tcp", servAddr)
+	ci.servAddr = "localhost:8080"
+	tcpAddr, err := net.ResolveTCPAddr("tcp", ci.servAddr)
 	if err != nil {
 		println("ResolveTCPAddr failed:", err.Error())
 		os.Exit(1)
 	}
-
-	conn, err := net.DialTCP("tcp", nil, tcpAddr)
+	State.conn, _ = net.DialTCP("tcp", nil, tcpAddr)
 	if err != nil {
 		println("Dial failed:", err.Error())
 		os.Exit(1)
 	}
 
 	wg.Add(1)
-	go tcpRead(conn, err)
-	//go tcpWrite(conn, err, message)
+	go tcpRead()
+	//go tcpWrite(conn) Don't need it here, old commit
 	wg.Wait()
 }
 
-func tcpRead(conn *net.TCPConn, err error) {
+func SendButtonPressed(keyPressed string) {
+	println("yooo i'm in, button pressed")
+	message := keyPressed + "=true"
+	go tcpWrite(message)
+}
+
+func tcpRead() {
+
+	c := SafeCounter{}
 	defer wg.Done()
 	for {
 		received := make([]byte, 1024)
-		_, err = conn.Read(received)
+		//println(State.conn)
+		_, err := State.conn.Read(received)
 		if err != nil {
 			println("Read from server failed:", err.Error())
 			os.Exit(1)
 		}
-		println(received)
+		c.Lock(string(received))
 	}
 }
 
-func tcpWrite(conn *net.TCPConn, err error, message string) {
-	_, err = conn.Write([]byte(message))
+func tcpWrite(message string) {
+	println(message)
+	_, err := State.conn.Write([]byte(message))
+	println("done")
 	if err != nil {
 		println("Write to server failed:", err.Error())
 		os.Exit(1)
